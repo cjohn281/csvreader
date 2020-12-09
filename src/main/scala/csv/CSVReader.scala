142bdc2049cd21a57405bca2ce079d93cae59d09
chris johnson <cjohn281@outlook.com>
2020-12-09 17:08:31 -0500

    First commit

---
+++ src/main/scala/csv/CSVReader.scala
@@ -0,0 +1,14 @@
+package csv
+
+/**
+  * CSVReader
+  * 
+  * 
+  */
+object CSVReader extends App {
+    println("State, Count")
+    val file = io.Source.fromFile("people.csv")
+    for (line <- file.getLines) {
+        println(line)
+    }
+}
